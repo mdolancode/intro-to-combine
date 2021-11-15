927c82b04b60a0d3e9003f92ea4753418d2e1a1b
Matthew Dolan <mdolan.code@gmail.com>
2021-11-15 06:10:05 -0800

    Import Combine. Create observer for APICaller. Set completion handler and get value. Set UI in tableView.

--- IntroToCombine/ViewController.swift
+++ IntroToCombine/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Combine
 
 class MyCustomTableViewCell: UITableViewCell {
     
@@ -18,22 +19,42 @@ class ViewController: UIViewController, UITableViewDataSource {
         tableView.register(MyCustomTableViewCell.self, forCellReuseIdentifier: "cell")
         return tableView
     }()
+    
+    private var models = [String]()
+    
+    var observer: AnyCancellable?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(tableView)
         tableView.dataSource = self
         tableView.frame = view.bounds
+        observer = APICaller.shared.fetchCompanies()
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                print("finished")
+            case .failure(let error):
+                print(error)
+            }
+        }, receiveValue: { [weak self] value in
+            self?.models = value
+            self?.tableView.reloadData()
+                
+        })
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableViewCell else {
             fatalError()
         }
+        
+        cell.textLabel?.text = models[indexPath.row]
         
         return cell
     }
