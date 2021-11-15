8cd41a879070a8f83d62d2ebcca65124c5896fd4
Matthew Dolan <mdolan.code@gmail.com>
2021-11-14 21:14:02 -0800

    Setup tableView and custom cell.

--- IntroToCombine/ViewController.swift
+++ IntroToCombine/ViewController.swift
@@ -7,13 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MyCustomTableViewCell: UITableViewCell {
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private let tableView: UITableView = {
+       let tableView = UITableView()
+        tableView.register(MyCustomTableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.frame = view.bounds
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? MyCustomTableViewCell else {
+            fatalError()
+        }
+        
+        return cell
+    }
 }
 
